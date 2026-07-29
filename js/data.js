596b2df886c85062e6d375c04d66bb75d20086c4
RicardoHdzm <jrhm95@gmail.com>
2026-07-29 01:11:41 -0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -326,5 +326,6 @@ export const GAMES = [
 ];
 
 // Próximos juegos (todavía sin jugar, sin marcador).
-export const SCHEDULE = [
-];
+// export const SCHEDULE = [
+//  { id: "s1", date: "2026-08-06", time: "19:00", opponent: "Pendiete" },
+// ];
