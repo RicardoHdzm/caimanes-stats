ce6861675684d3bc5c2acdc498bd94949aa8fd80
RicardoHdzm <jrhm95@gmail.com>
2026-08-16 16:47:12 -0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -373,5 +373,5 @@ export const GAMES = [
 
 // Próximos juegos (todavía sin jugar, sin marcador).
  export const SCHEDULE = [
-//  { id: "s1", date: "2026-08-06", time: "19:00", opponent: "Pendiete" },
+  { id: "s1", date: "2026-08-20", time: "19:00", opponent: "Softbol Valle Alto" },
 ];
